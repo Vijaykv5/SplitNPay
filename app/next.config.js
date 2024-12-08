9afda285bb19bbc005eeb2feb108a8c982e42215
vijay <vijaykv2228@gmail.com>
2024-12-09 02:59:03 +0530

    (fix) : Errors related to next.config.js

---
+++ app/next.config.js
@@ -0,0 +1,7 @@
+const nextConfig = {
+  webpack: config => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding')
+    return config
+  }
+}
+
--- app/next.config.mjs
+++
@@ -1,8 +0,0 @@
-const nextConfig = {
-  reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
-
-export default nextConfig;
